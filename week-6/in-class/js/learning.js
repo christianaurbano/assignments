ea5002cf24780c9a86083ca98aa45c8d97fcfdbb
christiana.urbano <christiana.urbano@gmail.com>
2018-03-20 20:48:18 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,35 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let myName = "Christy";
+let yourName = "Branden";
+
+if (yourName === "Christy") {
+  renderOutput("That's me!");
+} else if (yourName === "Branden") {
+  renderOutput("That's Branden!")
+} 
+  else {
+  renderOutput("I must be someone else.")
+}
+
+function helloWorld() {
+  renderOutput("Hello World")
+}
+
+helloWorld();
+helloWorld();
+helloWorld();
+
+function helloName(name) {
+  renderOutput ("Hello " + name);
+}
+
+helloName("Branden")
+
+function todaysHours(daysHours) {
+  renderOutput(daysHours);
+}
+
+
+hours.forEach(todaysHours);
