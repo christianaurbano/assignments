8e7c359642b8c20b4a261d87eea96a9f99c3e430
christiana.urbano <christiana.urbano@gmail.com>
2018-03-26 19:50:22 -0400

    dailyHours() assignment

--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,3 +4,25 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
+
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
+
+function dailyHours(hours) {
+  var daysHours = hours.split(": ");
+  if (daysHours[1] == "closed") {
+    renderOutput ("On " + daysHours[0] + " the library is " + daysHours[1]);
+  }
+  else {
+  renderOutput ("On " + daysHours[0] + " the hours are " + daysHours[1]);
+  }
+}
+
+hours.forEach(dailyHours);
